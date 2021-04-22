c12eaac292d165183c98cc969847f30845810342
Anna <garsinaanna32778@gmail.com>
2021-04-22 19:30:15 +0300

    created clock

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { Clock } from './components/Clock';
+
+class App extends React.Component {
+  state = {
+    isClockVisible: true,
+    clockName: 0,
+  }
+
+  setRandomName = () => {
+    this.setState({
+      clockName: Math.floor(Math.random() * 1000),
+    });
+  }
+
+  changeClockVisbility = () => {
+    this.setState(prevState => ({
+      isClockVisible: !prevState.isClockVisible,
+    }));
+  }
+
+  render() {
+    const { isClockVisible, clockName } = this.state;
+
+    return (
+      <>
+        <h1>React clock</h1>
+        {isClockVisible && <Clock name={clockName} />}
+
+        <>
+          <button
+            type="button"
+            onClick={this.changeClockVisbility}
+          >
+            {this.state.isClockVisible
+              ? 'Hide Clock'
+              : 'Show Clock'
+            }
+          </button>
+
+          <button
+            type="button"
+            onClick={this.setRandomName}
+          >
+            Set random name
+          </button>
+        </>
+      </>
+    );
+  }
+}
+
+export default App;
